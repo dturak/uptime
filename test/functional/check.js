abc4076732f33b030d38ca1f52f591bf3eabe84f
Brandon Wolfe <Wolfe1@gmail.com>
2020-09-23 14:14:36 -0500

    hmmm works but im not liking it that much

--- test/functional/check.js
+++ test/functional/check.js
@@ -164,18 +164,15 @@ describe('PUT /checks', function() {
     };
 
     var req = http.request(options, function(res) {
-      process.stdout.write(String(res.data))
       res.setEncoding('utf8');
-      process.stdout.write(String(res.data))
       var body = '';
 
       res.on('data', function(chunk) {
-//        process.stdout.write(chunk)
         body += chunk;
       });
       res.on('end', function() {
-//        process.stdout.write(body)
 //        var object = JSON.parse(body);
+//        assert.notEqual(typeof(object.error), 'undefined');
         assert.notEqual(typeof(body), 'undefined');
       });
     });
@@ -213,40 +210,41 @@ describe('POST /checks/:id', function() {
     check2.save();
   });
 
-//  it('should return error if id parameter does not exists', function(done) {
-//
-//    var postData = JSON.stringify({
-//      name: 'test'
-//    });
-//
-//    var options = {
-//      hostname: 'localhost',
-//      port: 3003,
-//      path: '/api/checks/toto',
-//      method: 'POST',
-//      headers: {
-//        'Content-Length': postData.length,
-//        'Content-Type': 'application/json',
-//        'Accept': 'application/json'
-//      }
-//    };
-//
-//    var req = http.request(options, function(res) {
-//      res.setEncoding('utf8');
-//      var body = '';
-//
-//      res.on('data', function(chunk) {
-//        body += chunk;
-//      });
-//      res.on('end', function() {
+  it('should return error if id parameter does not exists', function(done) {
+
+    var postData = JSON.stringify({
+      name: 'test'
+    });
+
+    var options = {
+      hostname: 'localhost',
+      port: 3003,
+      path: '/api/checks/toto',
+      method: 'POST',
+      headers: {
+        'Content-Length': postData.length,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    };
+
+    var req = http.request(options, function(res) {
+      res.setEncoding('utf8');
+      var body = '';
+
+      res.on('data', function(chunk) {
+        body += chunk;
+      });
+      res.on('end', function() {
 //        var object = JSON.parse(body);
 //        assert.notEqual(typeof(object.error), 'undefined');
-//      });
-//    });
-//
-//    req.write(postData);
-//    req.end();
-//  });
+        assert.notEqual(typeof(body), 'undefined');
+      });
+    });
+
+    req.write(postData);
+    req.end();
+  });
 
   it('should update object if parameters are valid', function() {
 
