90e2eea1cf185a25a033126b673d34025991b222
Brandon Wolfe <Wolfe1@gmail.com>
2020-09-23 13:45:13 -0500

    test

--- test/functional/check.js
+++ test/functional/check.js
@@ -228,15 +228,13 @@ describe('POST /checks/:id', function() {
     this.server = app.listen(3003);
   });
 
-  before(function() {
+  beforeEach(function() {
     check1 = new Check();
     check1.url = 'http://www.url1.fr';
     check1.name = 'name1';
     check1.isPaused = false;
     check1.save();
-  });
 
-  before(function() {
     check2 = new Check();
     check2.url = 'http://www.url2.fr';
     check2.isPaused = false;
