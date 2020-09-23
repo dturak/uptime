78f647089efa273827cfd96bc62ef93446a919f6
Brandon Wolfe <Wolfe1@gmail.com>
2020-09-23 14:26:58 -0500

    test

--- test/functional/check.js
+++ test/functional/check.js
@@ -145,41 +145,41 @@ describe('PUT /checks', function() {
     req.end();
   });
 
-  it('should not add an invalid element with no url', function() {
-    var postData = JSON.stringify({
-      name: 'test',
-      url: ''
-    });
-
-    var options = {
-      hostname: 'localhost',
-      port: 3003,
-      path: '/api/checks',
-      method: 'PUT',
-      headers: {
-        'Content-Length': postData.length,
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    };
-
-    var req = http.request(options, function(res) {
-      res.setEncoding('utf8');
-      var body = '';
-
-      res.on('data', function(chunk) {
-        body += chunk;
-      });
-      res.on('end', function() {
-//        var object = JSON.parse(body);
-//        assert.notEqual(typeof(object.error), 'undefined');
-        assert.notEqual(typeof(body), 'undefined');
-      });
-    });
-
-    req.write(postData);
-    req.end();
-  });
+//  it('should not add an invalid element with no url', function() {
+//    var postData = JSON.stringify({
+//      name: 'test',
+//      url: ''
+//    });
+//
+//    var options = {
+//      hostname: 'localhost',
+//      port: 3003,
+//      path: '/api/checks',
+//      method: 'PUT',
+//      headers: {
+//        'Content-Length': postData.length,
+//        'Content-Type': 'application/json',
+//        'Accept': 'application/json'
+//      }
+//    };
+//
+//    var req = http.request(options, function(res) {
+//      res.setEncoding('utf8');
+//      var body = '';
+//
+//      res.on('data', function(chunk) {
+//        body += chunk;
+//      });
+//      res.on('end', function() {
+////        var object = JSON.parse(body);
+////        assert.notEqual(typeof(object.error), 'undefined');
+//        assert.notEqual(typeof(body), 'undefined');
+//      });
+//    });
+//
+//    req.write(postData);
+//    req.end();
+//  });
 
   after(function() {
     Check.remove({});
@@ -210,41 +210,41 @@ describe('POST /checks/:id', function() {
     check2.save();
   });
 
-//  it('should return error if id parameter does not exists', function() {
-//
-//    var postData = JSON.stringify({
-//      name: 'test'
-//    });
-//
-//    var options = {
-//      hostname: 'localhost',
-//      port: 3003,
-//      path: '/api/checks/toto',
-//      method: 'POST',
-//      headers: {
-//        'Content-Length': postData.length,
-//        'Content-Type': 'application/json',
-//        'Accept': 'application/json'
-//      }
-//    };
-//
-//    var req = http.request(options, function(res) {
-//      res.setEncoding('utf8');
-//      var body = '';
-//
-//      res.on('data', function(chunk) {
-//        body += chunk;
-//      });
-//      res.on('end', function() {
-////        var object = JSON.parse(body);
-////        assert.notEqual(typeof(object.error), 'undefined');
-//        assert.notEqual(typeof(body), 'undefined');
-//      });
-//    });
-//
-//    req.write(postData);
-//    req.end();
-//  });
+  it('should return error if id parameter does not exists', function() {
+
+    var postData = JSON.stringify({
+      name: 'test'
+    });
+
+    var options = {
+      hostname: 'localhost',
+      port: 3003,
+      path: '/api/checks/toto',
+      method: 'POST',
+      headers: {
+        'Content-Length': postData.length,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    };
+
+    var req = http.request(options, function(res) {
+      res.setEncoding('utf8');
+      var body = '';
+
+      res.on('data', function(chunk) {
+        body += chunk;
+      });
+      res.on('end', function() {
+//        var object = JSON.parse(body);
+//        assert.notEqual(typeof(object.error), 'undefined');
+        assert.notEqual(typeof(body), 'undefined');
+      });
+    });
+
+    req.write(postData);
+    req.end();
+  });
 
   it('should update object if parameters are valid', function() {
 
