c0a7ba1125911a23b4911338f785976c2bf6b03a
Brandon Wolfe <Wolfe1@gmail.com>
2020-09-23 15:00:55 -0500

    test

--- test/functional/check.js
+++ test/functional/check.js
@@ -213,42 +213,42 @@ describe('POST /checks/:id', function() {
     check2.save();
   });
 
-//this one
-//  it('should return error if id parameter does not exists', function() {
-//
-//    var postData = JSON.stringify({
-//      name: 'test'
-//    });
-//
-//    var options = {
-//      hostname: 'localhost',
-//      port: 3003,
-//      path: '/api/checks/toto',
-//      method: 'POST',
-//      headers: {
-//        'Content-Length': postData.length,
-//        'Content-Type': 'application/json',
-//        'Accept': 'application/json'
-//      }
-//    };
-//
-//    var req = http.request(options, function(res) {
-//      res.setEncoding('utf8');
-//      var body = '';
-//
-//      res.on('data', function(chunk) {
-//        body += chunk;
-//      });
-//      res.on('end', function() {
-////        var object = JSON.parse(body);
-////        assert.notEqual(typeof(object.error), 'undefined');
-//        assert.notEqual(typeof(body), 'undefined');
-//      });
-//    });
-//
-//    req.write(postData);
-//    req.end();
-//  });
+//this one works but causes the process to continue for some reason
+  it('should return error if id parameter does not exists', function() {
+
+    var postData = JSON.stringify({
+      name: 'test'
+    });
+
+    var options = {
+      hostname: 'localhost',
+      port: 3003,
+      path: '/api/checks/toto',
+      method: 'POST',
+      headers: {
+        'Content-Length': postData.length,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    };
+
+    var req = http.request(options, function(res) {
+      res.setEncoding('utf8');
+      var body = '';
+
+      res.on('data', function(chunk) {
+        body += chunk;
+      });
+      res.on('end', function() {
+//        var object = JSON.parse(body);
+//        assert.notEqual(typeof(object.error), 'undefined');
+        assert.notEqual(typeof(body), 'undefined');
+      });
+    });
+
+    req.write(postData);
+    req.end();
+  });
 
   it('should update object if parameters are valid', function() {
 
@@ -285,6 +285,7 @@ describe('POST /checks/:id', function() {
 
     req.write(postData);
     req.end();
+    this.server.close();
   });
 
   it('should not throw error if called twice on same id', function() {
