ee0a7834445cc3dabb97ed05432176c2a63cd995
Brandon Wolfe <Wolfe1@gmail.com>
2020-09-23 09:07:09 -0500

    test, will fail

--- test/functional/check.js
+++ test/functional/check.js
@@ -1,6 +1,6 @@
 process.env.NODE_ENV = 'test';
 var mongoose = require('../../bootstrap');
-var Check = require('../../models/check');
+//var Check = require('../../models/check');
 var app = require('../../app');
 var assert = require('assert');
 var http = require('http');
