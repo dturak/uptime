0615cff7b79c4bb8275d458caf7c9bda985d30ec
Brandon Wolfe <Wolfe1@gmail.com>
2020-09-30 09:26:18 -0500

    cleanup

--- test/functional/check.js
+++ test/functional/check.js
@@ -1,13 +1,12 @@
 process.env.NODE_ENV = 'test';
-// var mongoose = require('../../bootstrap');
 var Check = require('../../models/check');
 var app = require('../../app');
 var assert = require('assert');
 var http = require('http');
 
-describe('GET /checks', function () {
+var check1, check2, pollerCollection; // fixtures
 
-    var check1, check2, pollerCollection; // fixtures
+describe('GET /checks', function () {
 
     before(function (done) {
         this.enableTimeouts(false)
@@ -152,12 +151,12 @@ describe('PUT /checks', function () {
     });
 
     it('should not add an invalid element with no url', function () {
-        var postData = JSON.stringify({
+        let postData = JSON.stringify({
             name: 'test',
             url: ''
         });
 
-        var options = {
+        let options = {
             hostname: 'localhost',
             port: 3003,
             path: '/api/checks',
@@ -169,9 +168,9 @@ describe('PUT /checks', function () {
             }
         };
 
-        var req = http.request(options, function (res) {
+        let req = http.request(options, function (res) {
             res.setEncoding('utf8');
-            var body = '';
+            let body = '';
 
             res.on('data', function (chunk) {
                 body += chunk;
@@ -193,8 +192,6 @@ describe('PUT /checks', function () {
 });
 
 describe('POST /checks/:id', function () {
-
-    var check1, check2, pollerCollection; // fixtures
 
     before(function () {
         this.enableTimeouts(false)
@@ -254,12 +251,12 @@ describe('POST /checks/:id', function () {
 
     it('should update object if parameters are valid', function () {
 
-        var postData = JSON.stringify({
+        let postData = JSON.stringify({
             name: 'test',
             url: 'http://newurl.test'
         });
 
-        var options = {
+        let options = {
             hostname: 'localhost',
             port: 3003,
             path: '/api/checks/' + check1.id,
@@ -271,15 +268,15 @@ describe('POST /checks/:id', function () {
             }
         };
 
-        var req = http.request(options, function (res) {
+        let req = http.request(options, function (res) {
             res.setEncoding('utf8');
-            var body = '';
+            let body = '';
 
             res.on('data', function (chunk) {
                 body += chunk;
             });
             res.on('end', function () {
-                var object = JSON.parse(body);
+                let object = JSON.parse(body);
                 assert.equal(object.name, 'test');
                 assert.equal(object.url, 'http://newurl.test');
             });
@@ -290,12 +287,12 @@ describe('POST /checks/:id', function () {
     });
 
     it('should not throw error if called twice on same id', function () {
-        var postData = JSON.stringify({
+        let postData = JSON.stringify({
             name: 'test',
             url: 'http://newurl.test'
         });
 
-        var options = {
+        let options = {
             hostname: 'localhost',
             port: 3003,
             path: '/api/checks/' + check1.id,
@@ -307,15 +304,15 @@ describe('POST /checks/:id', function () {
             }
         };
 
-        var req = http.request(options, function (res) {
+        let req = http.request(options, function (res) {
             res.setEncoding('utf8');
-            var body = '';
+            let body = '';
 
             res.on('data', function (chunk) {
                 body += chunk;
             });
             res.on('end', function () {
-                var object = JSON.parse(body);
+                let object = JSON.parse(body);
                 assert.equal(typeof (object.error), 'undefined');
                 assert.notEqual(typeof (object.name), 'undefined');
             });
@@ -324,10 +321,6 @@ describe('POST /checks/:id', function () {
         req.write(postData);
         req.end();
     });
-
-//  afterEach(function() {
-//    Check.remove({});
-//  });
 
     after(function () {
         Check.remove({});
