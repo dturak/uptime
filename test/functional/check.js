d906802473f75db5dd726f123f58a08930f29a6c
Brandon Wolfe <Wolfe1@gmail.com>
2020-09-30 09:42:36 -0500

    test

--- test/functional/check.js
+++ test/functional/check.js
@@ -35,7 +35,7 @@ describe('GET /checks', function () {
 
     it('should fetch all elements', function (done) {
 
-        var options = {
+        let options = {
             hostname: 'localhost',
             port: 3003,
             path: '/api/checks',
@@ -44,16 +44,16 @@ describe('GET /checks', function () {
             }
         };
 
-        var req = http.request(options, function (res) {
-            var body = "";
+        let req = http.request(options, function (res) {
+            let body = "";
             res.setEncoding('utf8');
             res.on('data', function (chunk) {
                 body += chunk;
             });
 
             res.on('end', function () {
-                content = JSON.parse(body);
-                assert.equal(content.length, 2);
+                let content = JSON.parse(body);
+                assert.strictEqual(content.length, 2);
                 done();
             });
         });
@@ -192,25 +192,6 @@ describe('PUT /checks', function () {
 });
 
 describe('POST /checks/:id', function () {
-
-    // before(function () {
-    //     this.enableTimeouts(false)
-    //     pollerCollection = app.get('pollerCollection');
-    //     this.server = app.listen(3003);
-    // });
-    //
-    // beforeEach(function () {
-    //     check1 = new Check();
-    //     check1.url = 'http://www.url1.fr';
-    //     check1.name = 'name1';
-    //     check1.isPaused = false;
-    //     check1.save();
-    //
-    //     check2 = new Check();
-    //     check2.url = 'http://www.url2.fr';
-    //     check2.isPaused = false;
-    //     check2.save();
-    // });
     before(function (done) {
         this.enableTimeouts(false)
         pollerCollection = app.get('pollerCollection');
@@ -236,42 +217,43 @@ describe('POST /checks/:id', function () {
         check2.save(done);
     });
 
-//this one works but causes the process to continue for some reason
-//  it('should return error if id parameter does not exists', function() {
-//
-//    var postData = JSON.stringify({
-//      name: 'test'
-//    });
-//
-//    var options = {
-//      hostname: 'localhost',
-//      port: 3003,
-//      path: '/api/checks/toto',
-//      method: 'POST',
-//      headers: {
-//        'Content-Length': postData.length,
-//        'Content-Type': 'application/json',
-//        'Accept': 'application/json'
-//      }
-//    };
-//
-//    var req = http.request(options, function(res) {
-//      res.setEncoding('utf8');
-//      var body = '';
-//
-//      res.on('data', function(chunk) {
-//        body += chunk;
-//      });
-//      res.on('end', function() {
-////        var object = JSON.parse(body);
-////        assert.notEqual(typeof(object.error), 'undefined');
-//        assert.notEqual(typeof(body), 'undefined');
-//      });
-//    });
-//
-//    req.write(postData);
-//    req.end();
-//  });
+// this one works but causes the process to continue for some reason
+     it('should return error if id parameter does not exists', function(done) {
+
+       let postData = JSON.stringify({
+         name: 'test'
+       });
+
+       let options = {
+         hostname: 'localhost',
+         port: 3003,
+         path: '/api/checks/toto',
+         method: 'POST',
+         headers: {
+           'Content-Length': postData.length,
+           'Content-Type': 'application/json',
+           'Accept': 'application/json'
+         }
+       };
+
+       let req = http.request(options, function(res) {
+         res.setEncoding('utf8');
+         let body = '';
+    
+         res.on('data', function(chunk) {
+           body += chunk;
+         });
+         res.on('end', function() {
+           let object = JSON.parse(body);
+           assert.notStrictEqual(typeof(object.error), 'undefined');
+    //        assert.notStrictEqual(typeof(body), 'undefined');
+           done();
+         });
+       });
+
+       req.write(postData);
+       req.end();
+     });
 
     it('should update object if parameters are valid', function (done) {
 
@@ -301,8 +283,8 @@ describe('POST /checks/:id', function () {
             });
             res.on('end', function () {
                 let object = JSON.parse(body);
-                assert.equal(object.name, 'test');
-                assert.equal(object.url, 'http://newurl.test');
+                assert.strictEqual(object.name, 'test');
+                assert.strictEqual(object.url, 'http://newurl.test');
                 done();
             });
         });
@@ -338,8 +320,8 @@ describe('POST /checks/:id', function () {
             });
             res.on('end', function () {
                 let object = JSON.parse(body);
-                assert.equal(typeof (object.error), 'undefined');
-                assert.notEqual(typeof (object.name), 'undefined');
+                assert.strictEqual(typeof (object.error), 'undefined');
+                assert.notStrictEqual(typeof (object.name), 'undefined');
                 done();
             });
         });
